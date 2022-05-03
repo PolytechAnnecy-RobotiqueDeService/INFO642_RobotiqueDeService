ce7a2922152eb90e73596137276f4327b5c93716
MarineSan <94964806+MarineSan@users.noreply.github.com>
2022-05-03 17:30:49 +0200

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,13 @@
-CREATE TABLE Media (
-	id_media INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	titre VARCHAR(255) NOT NULL,
-	type VARHCHAR(255),
-	lien VARCHAR(255))
+DROP TABLE IF EXISTS `media`;
 
+CREATE TABLE `media` (
+	`id_media` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`titre` VARCHAR(255) NOT NULL,
+	`type` VARCHAR(255),
+	`lien` VARCHAR(255));
+
+INSERT INTO `media` (`titre`, `type`, `lien`)
+VALUES ("test1_img", "image", "https://leblogauto.b-cdn.net/wp-content/uploads/2017/03/RenaultGroup_87805_global_fr.jpg");
+
+INSERT INTO `media` (`titre`, `type`, `lien`)
+VALUES ("test2_vidéo", "vidéo", "https://www.youtube.com/watch?v=YgtStCghUtg");
